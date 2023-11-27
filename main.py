5543f85ed7e3f0266ec889a173e67dca2ed68901
RichardSE97 <144933857+RichardSE97@users.noreply.github.com>
2023-11-26 22:51:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
-    ##################################################
-    """
+    female = int(input('Enter the number of female students:'))
+    male = int(input('Enter the number of male students:'))
+    total = female + male 
+    f_per = female * 100 / total 
+    m_per = male * 100 / total
 
-    """
+    print(f'Female Percentage is:\t{f_per:.2f}')
+    print(f'Male Percentage is: \t{m_per:.2f} ')
+    
     ########################################
     # Do not delete the return statement
     ########################################
